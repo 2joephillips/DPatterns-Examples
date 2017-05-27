6146da021a55550292faa4ff1144792abc27c42c
Joseph Phillips <joseph@josephephillips.com>
2017-05-27 16:48:34 -0400

    update layout

--- creational/builder/csharp/Program.cs
+++ creational/builder/csharp/Program.cs
@@ -124,7 +124,6 @@ namespace csharp
     {
         protected Vehicle vehicle;
         public Vehicle Vehicle => vehicle;
-
         public abstract void BuildFrame();
         public abstract void BuildEngine();
         public abstract void BuildWheels();
